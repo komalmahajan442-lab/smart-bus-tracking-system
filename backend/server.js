b2cb40763f4eef67d15ad1a847cb692ec539bd7b
komalmahajan442-lab <komalmahajan442@gmail.com>
2026-04-10 11:56:36 +0530

    added changes

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,16 @@ dotenv.config();
 const server=http.createServer(app);
 
 const io=new Server(server,
-    {cors:{origin:'*'}});
+    {cors:{
+      origin:'*',
+credentials:true
+    }});
 
 
- app.use(cors());
+ app.use(cors({
+  origin:"*",
+  credentials:true
+ }));
  app.use(express.json());
  app.use(router);
 app.use(adminRoutes);
